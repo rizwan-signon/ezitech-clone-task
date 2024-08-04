828afff4cc89dde724ab7c3554778b3a358af21b
your-username <your-email>
2024-08-04 22:50:40 +0500

    add internship component

---
+++ src/components/Internship.jsx
@@ -0,0 +1,286 @@
+import {
+  FaPhone,
+  FaInstagramSquare,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa";
+const Internship = () => {
+  return (
+    <div className="max-w-6xl mx-auto mt-24">
+      <h1 className=" text-center text-5xl uppercase">
+        perfect internship for you
+      </h1>
+      <div className=" grid grid-cols-1 sm:grid-cols-3 gap-6 mt-8">
+        <div className=" h-[400px] rounded-2xl  shadow-md">
+          <div className=" bg-blue-800 h-1/3 p-3 rounded-lg">
+            <h1 className=" text-3xl  underline underline-offset-8 font-bold text-white">
+              HTML?CSS?JS
+            </h1>
+            <p className="mt-3">ezitech</p>
+          </div>
+          <div className=" h-2/3 p-3 flex flex-col gap-3">
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaYoutube />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">hybrid</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                Rawalpinid
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaInstagramSquare />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                fulltime
+              </span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                part time
+              </span>
+            </div>
+            <button className=" w-1/3 mx-auto bg-blue-700 rounded-lg py-3 uppercase text-white mt-4 ">
+              text
+            </button>
+          </div>
+        </div>
+        <div className=" h-[400px] rounded-2xl  shadow-md">
+          <div className=" bg-blue-800 h-1/3 p-3 rounded-lg">
+            <h1 className=" text-3xl  underline underline-offset-8 font-bold text-white">
+              HTML?CSS?JS
+            </h1>
+            <p className="mt-3">ezitech</p>
+          </div>
+          <div className=" h-2/3  p-3 flex flex-col gap-3">
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">hybrid</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                Rawalpinid
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaPhone />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                fulltime
+              </span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                part time
+              </span>
+            </div>
+            <button className=" w-1/3 mx-auto bg-blue-700 rounded-lg py-3 uppercase text-white mt-4 ">
+              text
+            </button>
+          </div>
+        </div>
+        <div className=" h-[400px] rounded-2xl  shadow-md">
+          <div className=" bg-blue-800 h-1/3 p-3 rounded-lg">
+            <h1 className=" text-3xl  underline underline-offset-8 font-bold text-white">
+              HTML?CSS?JS
+            </h1>
+            <p className="mt-3">ezitech</p>
+          </div>
+          <div className=" h-2/3  p-3 flex flex-col gap-3">
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">hybrid</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                Rawalpinid
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaPhone />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                fulltime
+              </span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                part time
+              </span>
+            </div>
+            <button className=" w-1/3 mx-auto bg-blue-700 rounded-lg py-3 uppercase text-white mt-4 ">
+              text
+            </button>
+          </div>
+        </div>{" "}
+        <div className=" h-[400px] rounded-2xl  shadow-md">
+          <div className=" bg-blue-800 h-1/3 p-3 rounded-lg">
+            <h1 className=" text-3xl  underline underline-offset-8 font-bold text-white">
+              HTML?CSS?JS
+            </h1>
+            <p className="mt-3">ezitech</p>
+          </div>
+          <div className=" h-2/3 p-3 flex flex-col gap-3">
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaPhone />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">hybrid</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                Rawalpinid
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                fulltime
+              </span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                part time
+              </span>
+            </div>
+            <button className=" w-1/3 mx-auto bg-blue-700 rounded-lg py-3 uppercase text-white mt-4 ">
+              text
+            </button>
+          </div>
+        </div>{" "}
+        <div className=" h-[400px] rounded-2xl  shadow-md">
+          <div className=" bg-blue-800 h-1/3 p-3 rounded-lg">
+            <h1 className=" text-3xl  underline underline-offset-8 font-bold text-white">
+              HTML?CSS?JS
+            </h1>
+            <p className="mt-3">ezitech</p>
+          </div>
+          <div className=" h-2/3 p-3 flex flex-col gap-3">
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaPhone />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">hybrid</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                Rawalpinid
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                fulltime
+              </span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                part time
+              </span>
+            </div>
+            <button className=" w-1/3 mx-auto bg-blue-700 rounded-lg py-3 uppercase text-white mt-4 ">
+              text
+            </button>
+          </div>
+        </div>{" "}
+        <div className=" h-[400px] rounded-2xl  shadow-md">
+          <div className=" bg-blue-800 h-1/3 p-3 rounded-lg">
+            <h1 className=" text-3xl  underline underline-offset-8 font-bold text-white">
+              HTML?CSS?JS
+            </h1>
+            <p className="mt-3">ezitech</p>
+          </div>
+          <div className=" h-2/3 p-3 flex flex-col gap-3">
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">hybrid</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                Rawalpinid
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" bg-green-400 p-3 rounded-full">
+                <FaLinkedin />
+              </p>
+              <p> employemnt</p>
+              <span>-</span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                fulltime
+              </span>
+              <span className=" bg-green-200 px-2 py-1 rounded-lg">
+                part time
+              </span>
+            </div>
+            <button className=" w-1/3 mx-auto bg-blue-700 rounded-lg py-3 uppercase text-white mt-4 ">
+              text
+            </button>
+          </div>
+        </div>
+      </div>
+      <button className="text-center uppercase w-1/5 ml-[40%] mt-8 text-white bg-blue-700 p-3 rounded-lg hover:opacity-85">
+        Load more
+      </button>
+    </div>
+  );
+};
+
+export default Internship;
