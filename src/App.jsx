9a000d7cc498d25cb31391612f5eb8a179ccc31d
your-username <your-email>
2024-08-04 22:51:29 +0500

    add images and other stuff

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import HowItWorks from "./components/HowItWorks";
+import Home from "./pages/Home";
+import Courses from "./components/Courses";
+import Internship from "./components/Internship";
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <HowItWorks />
+      <Courses />
+      <Internship />
+    </div>
+  );
+};
+
+export default App;
